7791603958b179230e50263013c7a02b7fcc704f
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-10-15 23:07:43 +0900

    [ADD] 디스패처 모듈 추가

---
+++ data/src/main/java/com/navermovie/di/DispatcherModule.kt
@@ -0,0 +1,37 @@
+package com.navermovie.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DispatcherModule {
+    @Provides
+    @DispatcherIO
+    fun provideDispatcherIO(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @DispatcherMain
+    fun provideDispatcherMain(): CoroutineDispatcher = Dispatchers.Main
+
+    @Provides
+    @DispatcherDefault
+    fun provideDispatcherDefault(): CoroutineDispatcher = Dispatchers.Default
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class DispatcherIO
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class DispatcherMain
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class DispatcherDefault
+}
